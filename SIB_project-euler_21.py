512fa889646055e2136a15dc41c95cb2ffdd082e
ceceiliaallwein <ceceilia.allwein@gmail.com>
2014-01-12 15:58:21 -0500

    problems 21, 24

---
+++ SIB_project-euler_21.py
@@ -0,0 +1,52 @@
+'''
+
+PROJECT EULER
+
+Problem 21 - amicable numbers
+
+Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n). If d(a) = b and d(b) = a, where a != b, then a and b are an amicable pair and each of a and b are called amicable numbers. For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+Evaluate the sum of all the amicable numbers under 10000.
+'''
+
+
+def proper_divisor(a):
+    '''Sums proper divisors'''
+    output = 0 
+    for x in range (1,a-1):
+        result = a % x
+        if result == 0: output += x
+        else: 
+            pass
+    return output 
+
+
+def is_amicable(num1,num2):
+    '''Returns True if two given
+    integers are an amicable pair.'''
+    output = 0
+    sum1 = proper_divisor(num1)
+    sum2 = proper_divisor(sum1)
+    if num1 == sum2:
+        return True
+
+
+def sum_amicable(n):
+    '''Sums amicable numbers within 
+    a given upper bound of n.'''
+    # sets sum to zero
+    output = 0 
+    # sets range of input values 
+    for x in range(n):
+        # sums proper divisors of input value 
+        y = proper_divisor(x)
+        # evaluates if derived pair is amicable
+        if is_amicable (x,y) == True and x != y:
+            if y < n: 
+                # increments the output sum 
+                output = output + x
+    return output
+
+# Prints the sum of amicable numbers under 10,000
+print sum_amicable(10000)
+
---
+++ SIB_project-euler_24.py
@@ -0,0 +1,53 @@
+'''
+PROJECT EULER 
+
+24
+LEXICOGRAPHIC PERMUTATIONS
+A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. 
+
+If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+
+012   021   102   120   201   210
+
+What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+'''
+
+
+from itertools import permutations
+
+def make_list(x,y):
+	'''Creates a list from a given range.''' 
+	output = range(x,y+1)
+	return output 
+
+def format(perm):
+	'''Converts permutation, i.e. a list 
+	of ints, into single int.''' 
+	output = int(''.join(str(num) for num in perm))
+	return output
+
+
+def choose_permutation(x,y,n):
+	'''Generates n permutations from a list
+	of given range. Returns the n-th permutation. 
+	'''  
+	# Generates tuple of permutations
+	x = [permutations(make_list(x,y),r=None) for perm in range(n)]
+	# Converts tuple to list and selects n-th permutation
+	x = list(x[n-1])
+	x = x[n-1]
+	# Formats permutation as int
+	x = format(x)
+	return x
+
+# Prints the millionth lexocographical permutation of of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9
+print choose_permutation(0,9,1000000)
+
+
+
+
+
+
+
+
